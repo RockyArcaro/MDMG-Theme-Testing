e903bc1a67b18eded560f94a43545c2d05b0fe08
JoeChindemi <joechindemi@gmail.com>
2025-05-12 14:04:33 -0400

    Background color & center images
    Added a div to give full blue background color to "Our Partners", centered images in MadKo & Mad Dragon Records sections

--- about.php
+++ about.php
@@ -13,27 +13,32 @@ get_header(); ?>
     <h1>About MIP at Drexel</h1>
     <p>At Drexel University's Westphal College of Media Arts & Design, the Music Industry Program is a dynamic four-year undergraduate curriculum that bridges business, management, and technology within the music industry. Established in 2001, the program offers two specialized concentrations—Recording & Music Production (RAMP) and Music Business—empowering students to delve into both the creative and commercial sides of the field. Consistently ranked by Billboard Magazine as one of the top music business schools in the country, the program features renowned faculty such as Program Director Jeff Apruzzese, former Passion Pit bassist and founder of the Berklee Popular Music Institute, Tony Dofat, celebrated producer for artists like Mary J. Blige, Queen Latifah, and Will Smith, and Robert Weitzner, accomplished business executive and Founding Director of the American Association of Independent Music (A2IM).</p>
 </div>
-
-<section class="our-partners">
-  <div class="partners-text">
-    <h1>Our Partners</h1>
-    <p>
-      Since 2004, World Cafe Live has been at the heart of Philadelphia’s music scene. Created in partnership with WXPN, our venue quickly became a popular hub for music lovers, hosting local, national, and international acts. In 2008, we partnered with nonprofit LiveConnections to boost music education in the city, officially merging in 2019. Today, as one nonprofit, we continue to celebrate live music and inspire our community with memorable shows and programs.
-    </p>
-  </div>
-  <div class="partners-image">
-    <img src="images/partners-image.png" alt="Partner Graphic" />
-  </div>
-</section>
+<div class = "our-partners-color">
+    <section class="our-partners">
+    <div class="partners-text">
+        <h1>Our Partners</h1>
+        <p>
+        Since 2004, World Cafe Live has been at the heart of Philadelphia’s music scene. Created in partnership with WXPN, our venue quickly became a popular hub for music lovers, hosting local, national, and international acts. In 2008, we partnered with nonprofit LiveConnections to boost music education in the city, officially merging in 2019. Today, as one nonprofit, we continue to celebrate live music and inspire our community with memorable shows and programs.
+        </p>
+    </div>
+    <div class="partners-image">
+        <img src="images/partners-image.png" alt="Partner Graphic" />
+    </div>
+    </section>
+</div>
 
 <div class="about-content">
-    <img src = "/images/about-header-image.png" alt = "Mad Dragon Records Logo">
+    <div class = "flex-center">
+        <img src = "/images/about-header-image.png" alt = "Mad Dragon Records Logo">
+    </div>
     <h1>Mad Dragon Records</h1>
     <p>Mad Dragon Records, a division of MAD Dragon Music Group, is an independent, student-operated record label based out of Philadelphia’s Drexel University. The label supports emerging artists through several services, including marketing, distribution, and merchandise management. Mad Dragon Records is a hands-on, collaborative environment for students and artists, guiding projects from production through distribution while managing press, social media, and e-commerce strategies.</p>
 </div>
 
 <div class="about-content">
-<img src = "images/mkcpfp.png" alt = "MadKo Concerts Logo">
+    <div class = "flex-center">
+        <img src = "images/mkcpfp.png" alt = "MadKo Concerts Logo">
+    </div>
     <h1>MadKo Concerts</h1>
     <p>MadKo Concerts is a core component of MAD Dragon Music Group. Specializing in live event production, MadKo Concerts covers all aspects of event execution, from booking and talent relations to technical management, logistics, and marketing. Through their dedication to quality live production, MadKo ensures well managed, memorable events that showcases emerging talent and provide hands-on experience in the entertainment industry for Drexel students. 
     </p>
