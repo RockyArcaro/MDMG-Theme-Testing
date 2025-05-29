c759ae4444a8ea28a1a5defbdab5e36c77191773
RockyArcaro <rockyarcaro3@gmail.com>
2025-05-29 17:19:20 -0400

    Edited blog page, artists page, and store page to work and look cleaner

--- archive-artist.php
+++ archive-artist.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main class="artist-archive">
-  <h1>Our Artists</h1>
+  <h1 class="section-heading">Our Artists</h1>
 
   <div class="artist-grid">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template for Blog Posts Page (home.php)
+ */
+get_header(); ?>
+
+<main class="blog-archive">
+  <div class="container">
+  <h2 class="page-title">Latest Blog Posts</h2>
+  <div class="card-grid">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <a href="<?php the_permalink(); ?>" class="custom-blog-card">
+        <div class="card-image">
+          <?php if (has_post_thumbnail()) {
+            the_post_thumbnail('medium');
+          } ?>
+        </div>
+        <h3 class="card-title"><?php the_title(); ?></h3>
+      </a>
+    <?php endwhile; endif; ?>
+  </div>
+</div>
+</main>
+
+<?php get_footer(); ?>
