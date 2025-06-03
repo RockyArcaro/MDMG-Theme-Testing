f2b2ebcc26e008baee80867b7415de02792862d6
RockyArcaro <rockyarcaro3@gmail.com>
2025-06-03 18:37:45 -0400

    Edited content styles

--- archive-store_item.php
+++ archive-store_item.php
@@ -4,10 +4,10 @@
 <?php
   $external_link = get_field('external_link');
 ?>
-<a href="<?php echo esc_url($external_link); ?>" class="store-card" target="_blank" rel="noopener">
-  <div class="card">
+<a href="<?php echo esc_url($external_link); ?>" class="artist-card" target="_blank" rel="noopener">
+  <div>
     <?php if (has_post_thumbnail()): ?>
-      <?php the_post_thumbnail('medium'); ?>
+      <?php the_post_thumbnail('large'); ?>
     <?php endif; ?>
     <h3><?php the_title(); ?></h3>
     <h5><?php the_title(); ?></h5>
